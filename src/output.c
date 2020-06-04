2580446abed58f88b77baa7569e53d9589c85072
virgile <virgile.agnel@epitech.eu>
2020-06-04 02:46:54 +0200

    [add] can now combine options flags and it now display the right information according to the flag given as parameter to the program

--- src/output.c
+++ src/output.c
@@ -6,16 +6,56 @@
 
 #include "scsd.h"
 
+static void additional_info(file_list *file, global *stct)
+{
+    struct stat st = {0};
+    time_t date = 0;
+    char *result = NULL;
+
+    if (stct->values->fileinfo == DATE) {
+        stat(file->fullpath, &st);
+        date = st.st_mtime;
+        result = ctime(&date);
+        result[my_strlen(result) - 1] = 0;
+        my_printf("[%s] ", result);
+    }
+}
+
+static int dir_cases(file_list *tmp, global *stct, char *path)
+{
+    my_printf("\n");
+    additional_info(tmp, stct);
+    if (stct->values->print_full_path == true)
+        my_printf("%s\n", tmp->fullpath);
+    else
+        my_printf("%s\n", tmp->name);
+    if (scsd(stct, my_pathcat(path, tmp->name)) == 84)
+        return (84);
+    return (0);
+}
+
+static void reg_cases(file_list *tmp, global *stct)
+{
+    my_printf("\t");
+    additional_info(tmp, stct);
+    if (stct->values->print_full_path == true)
+        my_printf("%s\n", tmp->fullpath);
+    else
+        my_printf("%s\n", tmp->name);
+}
+
 int recurse_output(file_list *head, global *stct, char *path)
 {
     for (file_list *tmp = head; tmp != NULL; tmp = tmp->next) {
+        if (stct->values->show_hide == false && my_strncmp(".", tmp->name, 1) == 0)
+            continue;
         if (tmp->is_dir == true) {
-            my_printf("\n_%s\n", tmp->name);
-            if (scsd(stct, my_pathcat(path, tmp->name)) == 84)
+            if (dir_cases(tmp, stct, path) == 84)
                 return (84);
         }
-        else
-            my_printf("___%s\n", tmp->name);
+        else if (stct->values->list_dir_only == false) {
+            reg_cases(tmp, stct);
+        }
     }
     my_printf("\n");
     return (0);
